eed06ffae71a9bb705e8c8bb2cde40d714e76712
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-07-19 22:20:25 +0200

    added 03-components

---
+++ 03-components/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AnimatedClock from './components/AnimatedClock.vue';
+
+
+const italyValues = {
+  timeZone: 'Europe/Rome',
+  label: 'Italy',
+
+}
+
+const newYorkValues = {
+  timeZone: 'America/New_York',
+  label: 'New York',
+
+}
+
+</script>
+
+<template>
+  <div class="flex flex-grow items-center justify-center">
+    <div class="flex flex-col space-y-4">
+      <AnimatedClock v-bind="italyValues"></AnimatedClock>
+      <AnimatedClock v-bind="newYorkValues"></AnimatedClock>
+    </div>
+  </div>
+</template>
