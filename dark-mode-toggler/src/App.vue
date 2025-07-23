f058793f9bd023a6900a2651c4e04ec6b9e0bf20
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-07-23 09:29:59 +0200

    dark-mode-toggler

---
+++ dark-mode-toggler/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ToggleMode from './components/ToggleMode.vue';
+</script>
+
+<template>
+  <div class="p-4">
+    <ToggleMode />
+  </div>
+  <div class="px-4">
+    <p class="dark:text-white">Ciao mondo</p>
+  </div>
+</template>
